0f151d4395c7721bbb00edac91cee574dd413031
Žydrūnas Grabauskas <55529426+MotorEvil@users.noreply.github.com>
2020-02-26 18:15:50 +0200

    Code refactored

--- GildedRoseRefactoring/GildedRose.cs
+++ GildedRoseRefactoring/GildedRose.cs
@@ -14,78 +14,98 @@ namespace GildedRoseRefactoring
         {
             foreach (Item i in Items)
             {
-                if (i.Name != "Aged Brie" && i.Name != "Backstage passes to a TAFKAL80ETC concert" && i.Name != "Sulfuras, Hand of Ragnaros")
+                if (i.Name == "+5 Dexterity Vest" || i.Name == "Elixir of the Mongoose")
                 {
+                    i.SellIn -= 1; 
+
                     if (i.Quality > 0)
                     {
                         i.Quality -= 1;
-                        if (i.Name == "Conjured Mana Cake")
-                        {
-                            i.Quality -= 1;
-                        }
-                        if (i.SellIn <= 0)
-                        {
-                            if (i.Quality > 0)
-                            {
-                                i.Quality -= 1;
-                                if (i.Name == "Conjured Mana Cake")
-                                {
-                                    i.Quality -= 1;
-                                }
-                            }
-                        }
                     }
-                }
-                else
-                {
-                    if (i.Quality < 50 && i.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        i.Quality += 1;
 
-                        if (i.Name == "Aged Brie")
-                        {
-                            if (i.SellIn <= 0)
-                            {
-                                if (i.Quality < 50)
-                                {
-                                    i.Quality += 1;
-                                }
-
-                            }
-                        }
-
-                        if (i.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (i.SellIn < 11 && i.SellIn > 6)
-                            {
-                                if (i.Quality < 50)
-                                {
-                                    i.Quality += 1;
-                                }
-                            }
-
-                            if (i.SellIn < 6 && i.SellIn > 1)
-                            {
-                                if (i.Quality < 50)
-                                {
-                                    i.Quality += 2;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (i.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    i.SellIn -= 1;
                     if (i.SellIn < 0)
                     {
                         i.SellIn = 0;
-                        if (i.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            i.Quality = 0;
-                        }
 
+                        if (i.Quality > 0)
+                        {
+                            i.Quality -= 1;
+                        }
+                    }
+                }
+
+                if (i.Name == "Aged Brie")
+                {
+                    i.SellIn -= 1;
+
+                    if (i.Quality < 50)
+                    {
+                        i.Quality += 1;
+                    }
+
+                    if (i.SellIn < 0)
+                    {
+                        i.SellIn = 0;
+
+                        if (i.Quality < 50)
+                        {
+                            i.Quality += 1;
+                        }
+                    }
+                }
+
+                if (i.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    i.SellIn -= 1;
+
+                    if (i.SellIn > 0)
+                    {
+                        if (i.Quality < 50)
+                        {
+                            i.Quality += 1;
+                        }
+                    }
+
+                    if (i.SellIn < 6)
+                    {
+                        if (i.Quality < 50)
+                        {
+                            i.Quality += 1;
+                        }
+                    }
+
+                    if (i.SellIn < 11)
+                    {
+                        if (i.Quality < 50)
+                        {
+                            i.Quality += 1;
+                        }
+                    }
+
+                    if (i.SellIn < 0)
+                    {
+                        i.SellIn = 0; 
+                        i.Quality = 0;
+                    }
+                }
+
+                if (i.Name == "Conjured Mana Cake")
+                {
+                    i.SellIn -= 1;
+
+                    if (i.Quality > 0)
+                    {
+                        i.Quality -= 2;
+                    }
+
+                    if (i.SellIn < 0)
+                    {
+                        i.SellIn = 0;
+
+                        if (i.Quality > 0)
+                        {
+                            i.Quality -= 2;
+                        }
                     }
                 }
             }
